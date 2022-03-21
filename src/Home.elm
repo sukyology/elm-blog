5c6064150095c692c085a0c1beee4a824ec4349d
Suk kyo Suh <kyobe.brilliant24@gmail.com>
2022-03-21 23:47:18 +0900

    using cmd

--- src/Home.elm
+++ src/Home.elm
@@ -22,16 +22,16 @@ textFruit favorite fruit = if (fruit == favorite) then li [] [b [] [text fruit]]
 
 type UpdateMsg = ClickMsg {action: String, fruit: String} | ChangeMsg String
 
-update : UpdateMsg -> Model -> Model
+update : UpdateMsg -> Model -> (Model, Cmd UpdateMsg)
 update msg model = case msg of
     ClickMsg record ->
-        if record.action == "add" then {model | list = model.list ++ [record.fruit]} else model
+        if record.action == "add" then ({model | list = model.list ++ [record.fruit]}, Cmd.none) else (model, Cmd.none)
 
     ChangeMsg string ->
-        {model | content = string}
+        ({model | content = string}, Cmd.none)
 
 
-
+view: Model -> Html UpdateMsg
 view model = div [class "contents"] [
     input [type_ "text", placeholder "과일 입력하세요", value model.content, onInput ChangeMsg ] []
     ,  input [type_"button", value "추가", onClick (ClickMsg{action = "add", fruit = model.content})] []
@@ -42,8 +42,10 @@ view model = div [class "contents"] [
     ]
 
 --starting function always have to be named main
-main = Browser.sandbox
-    { init = initialModel
+main: Program () Model UpdateMsg
+main = Browser.element
+    { init = \_ -> (initialModel, Cmd.none)
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
     }
